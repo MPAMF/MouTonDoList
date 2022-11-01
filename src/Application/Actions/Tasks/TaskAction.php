cb6e1915649a09006e3d4388b5ac33862995a59b
SchawnnDev <iperskill@gmail.com>
2022-11-01 02:11:55 +0100

    added actions

---
+++ src/Application/Actions/Tasks/TaskAction.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Application\Actions\Tasks;
+
+use App\Application\Actions\Action;
+use App\Domain\Category\CategoryRepository;
+use App\Domain\Task\TaskRepository;
+use App\Domain\User\UserRepository;
+use Psr\Log\LoggerInterface;
+
+abstract class TaskAction extends Action
+{
+    protected UserRepository $userRepository;
+    protected TaskRepository $tempRepo;
+    protected CategoryRepository $categoryRepository;
+
+    public function __construct(LoggerInterface $logger, UserRepository $userRepository, TaskRepository $tempRepo,
+    CategoryRepository $categoryRepository)
+    {
+        parent::__construct($logger);
+        $this->userRepository = $userRepository;
+        $this->tempRepo = $tempRepo;
+        $this->categoryRepository = $categoryRepository;
+    }
+}
