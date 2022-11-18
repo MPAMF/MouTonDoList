21647d5eb3b963814d47e9976e906a0c03df517b
SchawnnDev <iperskill@gmail.com>
2022-11-18 21:56:10 +0100

    je pousse

--- src/Infrastructure/Security/Auth.php
+++ src/Infrastructure/Security/Auth.php
@@ -38,7 +38,7 @@ class Auth implements AuthInterface
     }
 
     /**
-     * @param mixed $user User Id or User object
+     * {@inheritDoc}
      */
     public function setUser(int|User $user) : void
     {
