78ea434802d4b06285cac1401d3c695a36fa33f6
SchawnnDev <iperskill@gmail.com>
2022-12-04 17:00:50 +0100

    fixed permission problems to get task

--- src/Application/Actions/Tasks/TaskAction.php
+++ src/Application/Actions/Tasks/TaskAction.php
@@ -44,19 +44,35 @@ abstract class TaskAction extends Action
     {
         $taskId = (int)$this->resolveArg('id');
 
+        if(!isset($with))
+        {
+            $with = ['category'];
+        } else if(!array_key_exists('category', $with))
+        {
+            $with[] = 'category';
+        }
+
         try {
             $task = $this->taskRepository->get($taskId, $with);
         } catch (TaskNotFoundException $e) {
             throw new HttpBadRequestException($this->request, $e->getMessage());
         }
 
+        $categoryId = $task->getCategory()->getParentCategoryId();
+
+        if($categoryId == null)
+        {
+            throw new HttpBadRequestException($this->request, $this->translator->trans('TaskNotFoundException'));
+        }
+
+
         if ($checkCanEdit) {
-            if (!$this->userCategoryRepository->exists(null, categoryId: $task->getCategoryId(),
+            if (!$this->userCategoryRepository->exists(null, categoryId: $categoryId,
                 userId: $this->user()->getId(), accepted: true, canEdit: true)) {
                 throw new HttpForbiddenException($this->request);
             }
         } else {
-            if (!$this->userCategoryRepository->exists(null, categoryId: $task->getCategoryId(),
+            if (!$this->userCategoryRepository->exists(null, categoryId: $categoryId,
                 userId: $this->user()->getId(), accepted: true)) {
                 throw new HttpForbiddenException($this->request);
             }
