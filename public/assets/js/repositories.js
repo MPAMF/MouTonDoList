d021c7ea609ea88b6a34d3f0df71ed5e74d54533
SchawnnDev <iperskill@gmail.com>
2022-12-04 17:05:02 +0100

    added repositories

--- public/assets/js/repositories.js
+++ public/assets/js/repositories.js
@@ -1,12 +1,10 @@
 class Repository {
 
-    constructor(endpoint)
-    {
+    constructor(endpoint) {
         this.endpoint = endpoint
     }
 
-    get(id)
-    {
+    get(id) {
         return $.ajax({
             url: `${this.endpoint}/${id}`,
             type: 'get',
@@ -14,8 +12,7 @@ class Repository {
         })
     }
 
-    put(id, data = {})
-    {
+    put(id, data = {}) {
         return $.ajax({
             url: `${this.endpoint}/${id}`,
             type: 'put',
@@ -25,8 +22,7 @@ class Repository {
         })
     }
 
-    post(data = {})
-    {
+    post(data = {}) {
         return $.ajax({
             url: this.endpoint,
             type: 'post',
@@ -36,8 +32,7 @@ class Repository {
         })
     }
 
-    delete(id)
-    {
+    delete(id) {
         return $.ajax({
             url: `${this.endpoint}/${id}`,
             type: 'delete',
@@ -50,28 +45,94 @@ class Repository {
 
 class TaskRepository extends Repository {
 
-    constructor()
-    {
+    constructor() {
         super('actions/tasks');
     }
 
-    get(id)
-    {
+    get(id) {
         return super.get(id)
     }
 
-    create(task)
-    {
+    create(task) {
         return super.post(task)
     }
 
-    update(task)
-    {
+    update(task) {
         return super.put(task.id, task)
     }
 
-    delete(task)
-    {
+    delete(task) {
         return super.delete(task.id)
+    }
+}
+
+class CategoryRepository extends Repository {
+
+    constructor() {
+        super('actions/categories');
+    }
+
+    get(id) {
+        return super.get(id)
+    }
+
+    create(category) {
+        return super.post(category)
+    }
+
+    update(category) {
+        return super.put(category.id, category)
+    }
+
+    delete(category) {
+        return super.delete(category.id)
+    }
+}
+
+class CommentRepository extends Repository {
+
+    constructor() {
+        super('actions/comments');
+    }
+
+    get(id) {
+        return super.get(id)
+    }
+
+    create(comment) {
+        return super.post(comment)
+    }
+
+    update(comment) {
+        return super.put(comment.id, comment)
+    }
+
+    delete(comment) {
+        return super.delete(comment.id)
+    }
+}
+
+class UserRepository extends Repository {
+
+    constructor() {
+        super('actions/users');
+    }
+
+    get(id) {
+        return super.get(id)
+    }
+
+    // Not implemented
+    create(user) {
+        return new Promise((resolve, reject) => reject());
+    }
+
+    update(user) {
+        return super.put(user.id, user)
+    }
+
+    // Not implemented
+    delete(user) {
+        return new Promise((resolve, reject) => reject());
     }
 }
