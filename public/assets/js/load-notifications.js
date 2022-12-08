defd05644475152c5a7794a1b9d0ad5d15c3b14e
qgerling <quentin.gerling@etu.unistra.fr>
2022-12-08 18:37:08 +0100

    load notification from server request

--- public/assets/js/load-notifications.js
+++ public/assets/js/load-notifications.js
@@ -1,15 +1,44 @@
+let invitationRepository = new InvitationRepository();
+let countErrors = 0;
+let notificationTab = [];
+
+function callNotification(){
+    invitationRepository.list()
+        .then(values => {
+            $("#btn-notification-count").html(values.data.length);
+            notificationTab = [];
+            values.data.forEach(value => {
+                notificationTab.push([value.category.name,value.category.owner,value.date]);
+            });
+            console.log(values);
+            countErrors = 0;
+        })
+        .catch(e => {
+            console.log(e);
+            countErrors++;
+        });
+
+    if(countErrors > 3){
+        setTimeout(callNotification,60000);
+    }
+    else{
+        setTimeout(callNotification,5000);
+    }
+}
+
 function loadTaskToDo(){
     let notifications = [];
     let numberDays = 3;
     let userId = data.id;
     let date = new Date();
-    date.setTime(date.getTime() - 24 * 3600 * 1000 * numberDays);
+    numberDays = 24 * 3600 * 1000 * numberDays;
 
     data.categories.forEach(function (userCategory) {
         if(userCategory.category.subCategories !== undefined){
             (userCategory.category.subCategories).forEach(function (subCategory) {
                 (subCategory.tasks).forEach(function (task) {
-                    if((task.assigned_id === userId) && (date.getTime() >= (new Date(task.due_date)).getTime())){
+                    console.log(userId)
+                    if((task.assigned_id === userId) && (date.getTime() - (new Date(task.due_date)).getTime() >= numberDays)){
                         notifications.push([task.name,task.category_id,task.id,task.due_date])
                     }
                 })
@@ -18,8 +47,4 @@ function loadTaskToDo(){
     })
 
     return notifications;
-}
-
-function loadNotification(){
-    return [["Math","Matthieu","2022-12-6"]];
 }
